2ecb50f5b6dcc245e9a2d7e0eabe61cfde9a462e
Zack McGhee <zack.mcghee@gmail.com>
2012-07-28 18:03:48 -0500

    Break CSS out to separate file

--- conf/urls.py
+++ conf/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
 	url(r'^$', 'filmlog.views.films_seen_this_year'),
 	url(r'^movies$', 'filmlog.views.films_seen_this_year'),
+	url(r'^movies/filmlog.css', 'django.views.generic.simple.direct_to_template', {'template': 'filmlog.css', 'mimetype': 'text/css'}),
 	url(r'^movies/(\d{4})/(\d{2})$', 'filmlog.views.films_seen_by_year'),
 	url(r'^movies/(\d{4})$', 'filmlog.views.films_seen_by_year'),
 	url(r'^movies/visual/(\d{4})/(\d{2})$', 'filmlog.views.html_month'),
