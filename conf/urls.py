bbab127973e38cd0f3d9ff730229b53f5718fc53
Zack McGhee <zack.mcghee@gmail.com>
2012-07-28 20:20:41 -0500

    Temp redirect root to about.me

--- conf/urls.py
+++ conf/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.shortcuts import redirect
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^$', 'filmlog.views.films_seen_this_year'),
+	url(r'^$', lambda r: redirect('http://about.me/zmcghee')),
 	url(r'^movies$', 'filmlog.views.films_seen_this_year'),
 	url(r'^movies/filmlog.css', 'django.views.generic.simple.direct_to_template', {'template': 'filmlog.css', 'mimetype': 'text/css'}),
 	url(r'^movies/(\d{4})/(\d{2})$', 'filmlog.views.films_seen_by_year'),
